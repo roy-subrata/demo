1f787e7b91ee141c4f5c53ae094e712bf1c8d6a4
roy-subrata <roy.subrata.cs@gmail.com>
2020-11-19 21:34:00 +0600

    remove line

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ namespace demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello Winter");
         }
     }
 }
